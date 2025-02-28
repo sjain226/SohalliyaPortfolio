1bf1c56951dc5ef7572a8dda36f69dd8368acc6b
sohalliya <jain.sohalliya@outlook.com>
2025-02-27 20:18:35 -0600

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/SohalliyaPortfolio/',
+//   build: {
+//     outDir: 'dist',
+//   },
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: "/SohalliyaPortfolio/", 
   plugins: [react()],
-  base: '/SohalliyaPortfolio/',
-  build: {
-    outDir: 'dist',
-  },
 });
